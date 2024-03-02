758151d89505fc91a5833e07626745c16c65839e
Lokesh Saipureddi <saipureddilokesh@gmail.com>
2024-03-02 13:40:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,43 +4,48 @@ Created on Wed Feb 12 21:24:17 2020
 
 @author: PC
 """
-
+import flask
 from flask import Flask, request
 import pickle
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+import sklearn
 
 app = Flask(__name__)
 
 # http://localhost:5000/api_predict
 
-model_pk = pickle.load(open("model-flower-v1.pkl","rb"))
+model_pk = pickle.load(open("flower_v1.pkl", "rb"))
 
 
-@app.route('/api_predict', methods = ['POST','GET'])
+@app.route('/api_predict', methods=['POST', 'GET'])
 def api_predict():
     if request.method == 'GET':
         return "Please Send POST Request"
     elif request.method == 'POST':
-        
+
         print("Hello" + str(request.get_json()))
-        
+
         data = request.get_json()
-        
+
         sepal_length = data["sepal_length"]
         sepal_width = data["sepal_width"]
         petal_length = data["petal_length"]
         petal_width = data["petal_width"]
-    
-        data = np.array([[sepal_length, sepal_width, 
+
+        data = np.array([[sepal_length, sepal_width,
                           petal_length, petal_width]])
-           
+
         prediction = model_pk.predict(data)
         return str(prediction)
 
+
 if __name__ == "__main__":
+    print(np.__version__)
+    print(flask.__version__)
+    print(sklearn.__version__)
     app.run()
-    
+
 '''    
 import requests
 
